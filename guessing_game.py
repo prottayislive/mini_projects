249fd30823429532a4625280742da3657186405a
Prottay Karim <70666023+prottayislive@users.noreply.github.com>
2021-05-31 00:53:14 +1000

    Update guessing_game.py

--- guessing_game.py
+++ guessing_game.py
@@ -5,6 +5,7 @@ def self_guess(x):
     """a function where user guesses the number"""
     random_number = random.randint(1, x)
     guess = 0
+    #loop to narrow down guesses
     while guess != random_number:
         guess = int(input(f"Guess a number between 1 and {x}: "))
         print(guess)
@@ -21,6 +22,7 @@ def computer_guess(y):
     lower = 1
     feedback = ''
     guess = ''
+    # loop to narrow down guesses
     while feedback != 'c':
         guess = random.randint(lower, upper)
         feedback = input(f"Is {guess} too high, low or correct?(enter: h/l/c)").lower().strip()
@@ -34,6 +36,7 @@ def computer_guess(y):
 print("Do you want to guess the number or should the computer guess your number?")
 game = input("Enter 'c' for computer guessing or anything else for your guess").lower().strip()
 upper_bound = int(input("Enter the highest number"))
+# contionals to choose game mode
 if game == 'c':
     computer_guess(upper_bound)
 else:
