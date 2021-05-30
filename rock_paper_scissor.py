5a5eea5212ae1697eb5714f36cf00d94efc5af4e
Prottay Karim <70666023+prottayislive@users.noreply.github.com>
2021-05-31 00:38:05 +1000

    rock_paper_scissors game Uploaded

---
+++ rock_paper_scissor.py
@@ -0,0 +1,27 @@
+import random
+
+
+def play():
+    """ Making initial choice for user and pc"""
+    print("Enter 'r' for rock, 'p' for paper, 's' for scissor")
+    user = input("Choose your move: ").lower().strip()
+    computer = random.choice(['r', 'p', 's'])
+    print(f"You played {user} and computer played {computer}")
+
+    if user == computer:
+        return 'tie'
+    if is_win(user, computer):
+        winner = "Congrats! You won!"
+        return winner
+    loser = "You Lost!"
+    return loser
+
+
+def is_win(player, opponent):
+    """Finding winner through logic --> r>s, s>p , p>r"""
+    if (player == 'r' and opponent == 's') or (player == 's' and opponent == 'p') or (
+            player == 'p' and opponent == 'r'):
+        return True
+
+
+print(play())
