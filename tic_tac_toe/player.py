8134f89d847ba3d83a8bad22c1fad94d80879b52
Prottay Karim <70666023+prottayislive@users.noreply.github.com>
2021-06-16 07:41:22 +0600

    added tic_tac_toe game

---
+++ tic_tac_toe/player.py
@@ -0,0 +1,37 @@
+import random
+import math
+
+
+class Player:
+    def __init__(self, letter):
+        self.letter = letter
+
+    def get_move(self, game):
+        pass
+
+
+class RandomComputerPlayer(Player):
+    def __init__(self, letter):
+        super().__init__(letter)
+
+    def get_move(self, game):
+        square = random.choice(game.available_moves())
+        return square
+
+
+class HumanPlayer(Player):
+    def __init__(self, letter):
+        super().__init__(letter)
+
+    def get_move(self, game):
+        valid_square = False
+        val = None
+        while not valid_square:
+            square = input(self.letter + "It's your turn (ENTER 0-9): ")
+            try:
+                val = int(square)
+                if val not in game.available_moves():
+                    raise ValueError
+                valid_square = True
+            except ValueError:
+                print('Try Again')
