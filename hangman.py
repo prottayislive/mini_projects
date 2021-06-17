750ceeba3ed49a7c77a43b134e30570e2fead911
Prottay Karim <70666023+prottayislive@users.noreply.github.com>
2021-06-18 03:13:59 +1000

    ​🌌​ Added comments and docstrings

--- hangman.py
+++ hangman.py
@@ -10,16 +10,19 @@ def get_word(word):
 
 
 def hangman():
+    """Game mechanics and playing"""
     word = get_word(word_list)
     word_letters = set(word)
     alphabet = set(string.ascii_uppercase)
     used_letters = set()
+    # loop to continue game until solving word
     while len(word_letters) > 0:
         print('Used:', ''.join(used_letters))
         list_letters = [letter if letter in used_letters else '-' for letter in word]
         print('Current word: ', ''.join(list_letters))
-        # getting input
+        # getting input for letters
         user_input = input('Guess a letter: ').upper()
+        # conditionals to match words with given word
         if user_input in alphabet - used_letters:
             used_letters.add(user_input)
             if user_input in word_letters:
