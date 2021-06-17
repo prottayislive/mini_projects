b3e020c29ee6fed814a596aacd2af57a12bb92a7
Prottay Karim <70666023+prottayislive@users.noreply.github.com>
2021-06-18 03:18:10 +1000

    🌌 Update comments

--- rock_paper_scissor.py
+++ rock_paper_scissor.py
@@ -8,6 +8,7 @@ def play():
     computer = random.choice(['r', 'p', 's'])
     print(f"You played {user} and computer played {computer}")
 
+    # conditionals to determine winner or loser
     if user == computer:
         return 'tie'
     if is_win(user, computer):
@@ -19,6 +20,7 @@ def play():
 
 def is_win(player, opponent):
     """Finding winner through logic --> r>s, s>p , p>r"""
+    # game mechanics to decide who wins
     if (player == 'r' and opponent == 's') or (player == 's' and opponent == 'p') or (
             player == 'p' and opponent == 'r'):
         return True
