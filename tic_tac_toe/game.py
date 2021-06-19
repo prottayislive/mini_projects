1222816288914e627a127f410538f807fb5dfae7
Prottay Karim <70666023+prottayislive@users.noreply.github.com>
2021-06-20 04:54:07 +1000

    🌌 Added Comments

--- tic_tac_toe/game.py
+++ tic_tac_toe/game.py
@@ -4,20 +4,24 @@ from player import HumanPlayer, RandomComputerPlayer
 
 
 class TicTacToe():
+    """ class to define the full game mecahnics"""
     def __init__(self):
         self.board = self.make_board()
         self.current_winner = None
 
     @staticmethod
+    """ making a skeleton of the board"""
     def make_board():
         return [' ' for _ in range(9)]
 
     def print_board(self):
+        """ showing perfect board """
         for row in [self.board[i*3:(i+1) * 3] for i in range(3)]:
             print('| ' + ' | '.join(row) + ' |')
 
     @staticmethod
     def print_board_nums():
+        """showing which numbers to enter for which box"""
         # 0 | 1 | 2
         number_board = [[str(i) for i in range(j*3, (j+1)*3)] for j in range(3)]
         for row in number_board:
